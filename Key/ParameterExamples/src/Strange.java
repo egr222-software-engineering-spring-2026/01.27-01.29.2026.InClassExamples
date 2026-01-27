94973d12ca7b17e08ff88d1a29ff27a05c670091
Larry Clement <lclement@calbaptist.edu>
2026-01-26 18:40:06 -0800

    After adding Strange

---
+++ Key/ParameterExamples/src/Strange.java
@@ -0,0 +1,14 @@
+public class Strange {
+    public static void strange(int x) {
+        x++;
+        System.out.println("1.x = " + x);
+    }
+
+    public static void main(String[] args) {
+        int x = 23;
+        strange(x);
+        System.out.println("2.x = " + x);
+
+        // other code here
+    }
+}
